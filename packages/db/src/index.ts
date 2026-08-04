504b9dec7a024531d285b24eb19b061c550841e7
llaxmi <ll4xmi@gmail.com>
2026-08-05 00:09:01 +0545

    Enhance exports in core and db modules: add missing cost and faithfulness metrics, and include additional document and run-related functions

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -8,10 +8,14 @@ export {
   type MatchedChunk,
   type ChunkRow,
 } from "./vector";
-export { upsertDocument, type DocumentRow } from "./documents";
+export { upsertDocument, getDocumentSha256, type DocumentRow } from "./documents";
 export { upsertQuestions, getQuestions, type QuestionRow } from "./questions";
 export {
   getCompletedRunKeys,
   writeRun,
+  getBackfillRows,
+  updateEvalMetrics,
   type RunInsert,
+  type BackfillRow,
+  type EvalMetricPatch,
 } from "./runs";
